ebf70e30d073cf7853451771ef31e365f86df91e
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-18 18:28:41 -0400

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 //Re-export stuff from errors and middlewares
+
 export * from './errors/badRequestError';
 export * from './errors/customError';
 export * from './errors/databaseConnectionError';
 export * from './errors/notAuthorizedError';
 export * from './errors/notFoundError';
 export * from './errors/requestValidationError';
+
 export * from './middlewares/currentUser';
 export * from './middlewares/errorHandler';
 export * from './middlewares/requireAuth';
 export * from './middlewares/validateRequest';
+
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/types/subjects';
+export * from './events/types/ticket-created-event';
+export * from './events/types/ticket-updated-event';
